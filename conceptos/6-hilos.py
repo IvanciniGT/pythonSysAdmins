9419c80d82d500145b2962ba030be051b1226072
Ubuntu <ubuntu@ip-172-31-5-103.eu-west-1.compute.internal>
2021-07-13 09:35:53 +0000

    hilos 1

---
+++ conceptos/6-hilos.py
@@ -0,0 +1,5 @@
+# Quiero un tipo nuevo llamado Contador
+# Un contador debe tener asociado un nombre
+# También, hasta cuando contar, y cuanto tiempo espera entre numero y numero
+# Quiero que esta clase tenga un metodo contar <<< Aqui empieza a contar
+                                                                            # Quiero tener un metodo que sea detener Cuenta
