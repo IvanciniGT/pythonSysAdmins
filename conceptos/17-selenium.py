aac44efc977f06df6c2d4224965c2d18ef4de710
Ubuntu <ubuntu@ip-172-31-5-103.eu-west-1.compute.internal>
2021-07-16 10:43:30 +0000

    selenium

--- conceptos/17-selenium.py
+++ conceptos/17-selenium.py
@@ -2,31 +2,30 @@ from selenium import webdriver
 import re
 
 driver=webdriver.Remote(
-        command_executor="http://localhost:4444",
+        command_executor="http://localhost:4444/wd/hub",
         desired_capabilities={
-                "browserName": "firefox",
-                "browserVersion": "90.0",
-                "platformName": "Linux",
-                "se:vncEnabled": True        
+                "browserName": "firefox"
         }
     )
 
-driver.implictly_wait(5)            # Timeout de cada operacion
+try:
+    driver.implicitly_wait(10)
+    
+    driver.get("http://www.itnow.es/")
+    print(driver.title)
 
-driver.get("http://itnow.es/")
-
-# Buscar un elemento dentro de la web.....                      Y hacemos algo en el
-driver.find_element_by_xpath("//input[@id='txtBuscar']")                            .send_keys("servicios")
-driver.find_element_by_xpath("//input[@id='ctl00_BuscadorMenu1_bt_search']")        .click()
-#try:
-#    driver.find_element_by_xpath("//h2[contains(text(),'Error')]") 
-#except: # Se produce si el elemento no existe
-texto_del_h2=driver.find_element_by_xpath("//h2[contains(@class,'ms-search-header')]") .text
-
-if re.match(".*error",texto_del_h2.lower()):
-    print("La busqueda de la web no esta funcionando")
-else:
-    print("la busqueda de la web si que funciona")
-
-print(driver.title)
-driver.quit()
+    # Buscar un elemento dentro de la web.....                      Y hacemos algo en el
+    driver.find_element_by_xpath("//input[@id='txtBuscar']")                            .send_keys("servicios")
+    driver.find_element_by_xpath("//input[@id='ctl00_BuscadorMenu1_bt_search']")        .click()
+    #try:
+    #    driver.find_element_by_xpath("//h2[contains(text(),'Error')]") 
+    #except: # Se produce si el elemento no existe
+    texto_del_h2=driver.find_element_by_xpath("//h2[contains(@class,'ms-search-header')]") .text
+    print(texto_del_h2)
+    if re.match(".*error",texto_del_h2.lower()):
+        print("La busqueda de la web no esta funcionando")
+    else:
+        print("la busqueda de la web si que funciona")
+    
+finally:
+    driver.quit()
