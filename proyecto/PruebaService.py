e544069873ba8fe62766e4a5b1e5eb7f4ce25586
Ubuntu <ubuntu@ip-172-31-5-103.eu-west-1.compute.internal>
2021-07-16 07:22:20 +0000

    fabric

--- proyecto/PruebaService.py
+++ proyecto/PruebaService.py
@@ -11,20 +11,22 @@ class PruebaService(Prueba):
     def __init__(self, nombre=None, timeout=0, intervalo=0, numero_fallos_permitidos_consecutivos=0, servicios=() ):
         super().__init__(nombre, timeout, intervalo, numero_fallos_permitidos_consecutivos)
         self.servicios=servicios
+
+    def ejecutar(self, servidor):
+        
         with open("services.jinja") as archivo_plantilla:
             texto_de_la_plantilla=archivo_plantilla.read()
         self.plantilla=Template( texto_de_la_plantilla )
         
-    def ejecutar(self, servidor):
         resultado=self.plantilla.render( self.__dict__ )
 
         os.environ['use_ssh_config']="True"
         os.environ['key_filename']="~/.ssh/id_rsa"
-        remoto=Connection(host= "root@servidor")
+        remoto=Connection(host= self.usuario"@"+servidor)
         # Vamos a guardar la plantilla en un fichero temporal.. que vamos a copiar dentro del remoto
         
-        subproceso=remoto.run( resultado , stdout=PIPE, stderr=PIPE)
-        return subproceso.returncode == 0
+        respuesta=remoto.run( resultado )
+        return respuesta.return_code == 0
         
         #if subproceso.resturncode == 0:
         #    return True
